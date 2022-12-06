762cdbaa5dd99d2121d928d03f9919e495254790
Patrickdias40 <patrickgabrieldias40@gmail.com>
2022-12-06 16:05:58 -0300

    Colocando os arquivos

---
+++ Truquinho/app/src/main/java/com/example/truquinho/Nova_Partida.java
@@ -0,0 +1,14 @@
+package com.example.truquinho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nova_Partida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nova_partida);
+    }
+}
---
+++ Truquinho/app/src/main/java/com/example/truquinho/Partidas.java
@@ -0,0 +1,14 @@
+package com.example.truquinho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Partidas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.partidas);
+    }
+}
