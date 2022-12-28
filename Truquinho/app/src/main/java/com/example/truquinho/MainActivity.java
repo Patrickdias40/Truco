bff4686e2ea2c464542a6fda05382a0fcab384ae
guikage <guieinloft@protonmail.com>
2022-12-27 22:52:40 -0300

    fazendo o contador funcionar

--- Truquinho/app/src/main/java/com/example/truquinho/MainActivity.java
+++ Truquinho/app/src/main/java/com/example/truquinho/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.truquinho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button regras;
+    Button partidas;
+    Button rapida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        regras = findViewById(R.id.bt_Regras);
+        partidas = findViewById(R.id.bt_Partida);
+        rapida = findViewById(R.id.rapida);
+
+        regras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Regras.class);
+                startActivity(intent);
+            }
+        });
+        partidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Historico.class);
+                startActivity(intent);
+            }
+        });
+        rapida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Partidas.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Truquinho/app/src/main/java/com/example/truquinho/Partidas.java
+++ Truquinho/app/src/main/java/com/example/truquinho/Partidas.java
@@ -3,12 +3,56 @@ package com.example.truquinho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Partidas extends AppCompatActivity {
+
+    int pontos_nos = 0;
+    int pontos_eles = 0;
+
+    TextView texto_nos;
+    TextView texto_eles;
+
+    Button mais_nos;
+    Button mais_eles;
+    Button menos_nos;
+    Button menos_eles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.partidas);
+        texto_nos = findViewById(R.id.pontos_nos);
+        texto_eles = findViewById(R.id.pontos_eles);
+        mais_nos = findViewById(R.id.mais_nos);
+        mais_eles = findViewById(R.id.mais_eles);
+        menos_nos = findViewById(R.id.menos_nos);
+        menos_eles = findViewById(R.id.menos_eles);
+
+        texto_nos.setText(String.valueOf(pontos_nos));
+        texto_eles.setText(String.valueOf(pontos_eles));
+
+        mais_nos.setOnClickListener(this::adiciona_nos);
+        mais_eles.setOnClickListener(this::adiciona_eles);
+        menos_nos.setOnClickListener(this::retira_nos);
+        menos_eles.setOnClickListener(this::retira_eles);
+    }
+    public void adiciona_nos(View view){
+        pontos_nos += 1;
+        texto_nos.setText(String.valueOf(pontos_nos));
+    }
+    public void adiciona_eles(View view){
+        pontos_eles += 1;
+        texto_eles.setText(String.valueOf(pontos_eles));
+    }
+    public void retira_nos(View view){
+        pontos_nos -= 1;
+        texto_nos.setText(String.valueOf(pontos_nos));
+    }
+    public void retira_eles(View view){
+        pontos_eles -= 1;
+        texto_eles.setText(String.valueOf(pontos_eles));
     }
 }
